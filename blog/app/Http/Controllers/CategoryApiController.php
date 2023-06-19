38507fee72fbcefe791996e8b682df32bbe5f8af
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2023-06-19 21:09:03 +0630

    [Added] API Controller

---
+++ blog/app/Http/Controllers/CategoryApiController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryApiController extends Controller
+{
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->name == "") {
+            return response(["msg" => "Name required"], 400);
+        }
+
+        $category = new Category;
+        $category->name = $request->name;
+        $category->save();
+
+        return $category;
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        if ($request->name == "") {
+            return response(["msg" => "Name required"], 400);
+        }
+
+        $category->name = $request->name;
+        $category->save();
+
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return $category;
+    }
+}
