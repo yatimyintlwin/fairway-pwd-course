254a1bcf3dc610c47db083ca5d61c9a5ca30ab04
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2023-06-25 16:53:56 +0630

    [Completed] Basic API with Laravel

--- blog/app/Http/Controllers/CategoryApiController.php
+++ blog/app/Http/Controllers/CategoryApiController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class CategoryApiController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
     public function index()
     {
         return Category::all();
--- blog/routes/api.php
+++ blog/routes/api.php
@@ -7,6 +7,24 @@ use App\Http\Controllers\CategoryApiController;
 
 Route::apiResource('/categories', CategoryApiController::class);
 
+Route::post('/login', function () {
+    $email = request()->email;
+    $password = request()->password;
+
+    if (!$email or !$password) {
+        return response(["msg" => "email or password incorrect"],  401);
+    }
+
+    $user = \App\Models\User::where("email", $email)->first();
+    if ($user) {
+        if (password_verify($password, $user->password)) {
+            return $user->createToken('api')->plainTextToken;
+        }
+    }
+
+    return response(["msg" => "email or password incorrect"], 401);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
